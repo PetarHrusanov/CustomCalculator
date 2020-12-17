9739ce9c8239b56196b1ba1ea325a6c01d15b295
PetarHrusanov <59975974+PetarHrusanov@users.noreply.github.com>
2020-12-17 23:32:04 +0200

    Basic Navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { NgModule } from '@angular/core';
 import Backendless from 'backendless';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { CoreModule } from './core/core.module';
+
 Backendless.initApp(environment.backendless.APP_ID, environment.backendless.API_KEY);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
